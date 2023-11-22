6f62c1db33fd7902e01d82ee6995f2c41f393f73
The_Mixa <misha.oleg548@gmail.com>
2023-11-22 20:45:03 +0500

    Работающий вариант main.py

--- code/main.py
+++ code/main.py
@@ -14,22 +14,88 @@ def start(message):
     bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
 
 
+@bot.message_handler()
+def choise6(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.row_width = 1
+    btn = types.InlineKeyboardButton('Назад', callback_data="yes")
+    markup.add(btn)
+    with open('../texts/young_voter_competition.txt', encoding='utf-8') as file:
+        send_mess = file.read()
+
+    bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
+
+@bot.message_handler()
+def choise5(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.row_width = 1
+    btn = types.InlineKeyboardButton('Назад', callback_data="yes")
+    markup.add(btn)
+    with open('../texts/UMNIC_comprtition.txt', encoding='utf-8') as file:
+        send_mess = file.read()
+
+    bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
+
+@bot.message_handler()
+def choise4(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.row_width = 1
+    btn = types.InlineKeyboardButton('Назад', callback_data="yes")
+    markup.add(btn)
+    with open('../texts/residents_competition.txt', encoding='utf-8') as file:
+        send_mess = file.read()
+
+    bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
+
+@bot.message_handler()
+def choise3(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.row_width = 1
+    btn = types.InlineKeyboardButton('Назад', callback_data="yes")
+    markup.add(btn)
+    with open('../texts/public_services.txt', encoding='utf-8') as file:
+        send_mess = file.read()
+
+    bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
+
+@bot.message_handler()
+def choise2(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.row_width = 1
+    btn = types.InlineKeyboardButton('Назад', callback_data="yes")
+    markup.add(btn)
+    with open('../texts/accelerator_competition.txt', encoding='utf-8') as file:
+        send_mess = file.read()
+
+    bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.data == 'yes':
         about(call.message)
+    elif call.data == 'choise6':
+        choise6(call.message)
+    elif call.data == 'choise5':
+        choise5(call.message)
+    elif call.data == 'choise4':
+        choise4(call.message)
+    elif call.data == 'choise3':
+        choise3(call.message)
+    elif call.data == 'choise2':
+        choise2(call.message)
 
 
 @bot.message_handler(commands=['about'])
 def about(message):
     markup = types.InlineKeyboardMarkup()
     markup.row_width = 1
-    btn1 = types.InlineKeyboardButton('Ближайшие мероприятия', callback_data="NumberOne")
-    btn2 = types.InlineKeyboardButton('Государственные услуги', callback_data="NumberOne")
-    btn3 = types.InlineKeyboardButton('Как стать резидентом Технопарка Югры', callback_data="NumberOne")
-    btn4 = types.InlineKeyboardButton('Акселератор технологических стартапов', callback_data="NumberOne")
-    btn5 = types.InlineKeyboardButton('Конкурс «УМНИК»', callback_data="NumberOne")
-    btn6 = types.InlineKeyboardButton('Конкурс «Молодой изобретатель Югры»', callback_data="NumberOne")
+    btn1 = types.InlineKeyboardButton('Ближайшие мероприятия', callback_data="choise1")
+    btn2 = types.InlineKeyboardButton('Государственные услуги', callback_data="choise2")
+    btn3 = types.InlineKeyboardButton('Как стать резидентом Технопарка Югры', callback_data="choise3")
+    btn4 = types.InlineKeyboardButton('Акселератор технологических стартапов', callback_data="choise4")
+    btn5 = types.InlineKeyboardButton('Конкурс «УМНИК»', callback_data="choise5")
+    btn6 = types.InlineKeyboardButton('Конкурс «Молодой изобретатель Югры»', callback_data="choise6")
     markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
     send_mess = f"Что Вас интересует?"
     bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
