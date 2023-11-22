da8aaa9783201cb2e282c3762d7e03484ec33e80
dast3rr <129662556+dast3rr@users.noreply.github.com>
2023-11-22 21:13:20 +0500

    Update get_info.py

--- parser/get_info.py
+++ parser/get_info.py
@@ -18,18 +18,24 @@ class GetInfo:
             pass
 
     def closest_events(self):
+        events = ['']
         response = requests.get('https://www.tp86.ru/press-centr/events/')
         soup = BeautifulSoup(response.text, 'html.parser')
         data = soup.findAll('a', class_='grid events__list_item')
         for el in data:
+            event = ''
             date = el.find('p', class_='day text-color-black font-montserrat-weight-700 font-size-36').text.strip()
             month = el.find('p', class_='month').text.strip()
             text = el.find('p', class_='prew-text text-color-black font-myriad-pro-weight-600 font-size-24 '
                                        'text-color-dark-purple events__list_item_prew-text').text.strip()
             more_info_url = el['href']
-            print(date, month)
-            print(text)
-            print(f'https://www.tp86.ru{more_info_url}', end='\n\n')
+            event += date + ' ' + month + '\n'
+            event += text + '\n'
+            event += f'https://www.tp86.ru{more_info_url}' + '\n\n'
+            events += event
+        with open('texts/events.txt', 'w', encoding='utf8') as f:
+            for event in events:
+                f.write(event)
 
     def services(self):
         response = requests.get('https://www.tp86.ru/services/services/')
