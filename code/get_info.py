8191dcb77263dfa7fb74a8b74c87660299fd7328
The_Mixa <misha.oleg548@gmail.com>
2023-11-23 17:04:29 +0500

    Небольшие поправки 1 абзац --> 3 абзаца

--- code/get_info.py
+++ code/get_info.py
@@ -68,7 +68,7 @@ class GetInfo:
         name = name[0].text.strip()
         finish_news += f'*{name}*\n'
 
-        paragraphs_find = soup_news.find('div', class_='news-detail__block_text line-height-200').findAll('p')
+        paragraphs_find = soup_news.findAll('div', class_='news-detail__block_text line-height-200').findAll('p')
         for paragraph in paragraphs_find[:3]:
             text = paragraph.text.strip()
             finish_news += f'{text}\n'
