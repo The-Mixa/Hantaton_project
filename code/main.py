b05c326eb299431d8e3842c248af5e7c13a9d1dc
slyvk <slyvkanikitao@gmail.com>
2023-11-24 12:06:38 +0500

    Исправлен баг с /about, добавлен ответ бота на случайное сообщение от пользователя

--- code/main.py
+++ code/main.py
@@ -3,8 +3,9 @@ from telebot import types
 from get_info import GetInfo
 import time
 
+
 bot = telebot.TeleBot('6627972348:AAELm5jh-LOE_MYq8mrd-FATGzOQmWqEHc8')
-channel_id = "@Technopark_of_Ugra"
+channel_id = "@test_channel_Saltykov_detachment"
 
 
 @bot.message_handler(commands=['start'])
@@ -20,27 +21,65 @@ def start(message):
     bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
 
 
+@bot.message_handler(commands=['about'])
+def about(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.row_width = 1
+    btn1 = types.InlineKeyboardButton('Ближайшие мероприятия', callback_data="choise1")
+    btn2 = types.InlineKeyboardButton('Государственные услуги', callback_data="choise2")
+    btn3 = types.InlineKeyboardButton('Как стать резидентом Технопарка Югры', callback_data="choise3")
+    btn4 = types.InlineKeyboardButton('Акселератор технологических стартапов', callback_data="choise4")
+    btn5 = types.InlineKeyboardButton('Конкурс «УМНИК»', callback_data="choise5")
+    btn6 = types.InlineKeyboardButton('Конкурс «Молодой изобретатель Югры»', callback_data="choise6")
+    markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
+
+    send_mess = f"Что Вас интересует?"
+    bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'], func=lambda call: True)
+def retur_func(message):
+    bot.send_message(message.chat.id, 'Воспользуйтесь командой *«/about»*, чтобы начать...', parse_mode='Markdown')
+
+
 @bot.message_handler()
-def choise6(message):
+def choise1(message):
     markup = types.InlineKeyboardMarkup()
     markup.row_width = 1
     btn = types.InlineKeyboardButton('Назад', callback_data="yes")
     markup.add(btn)
 
-    with open('../texts/young_voter_competition.txt', encoding='utf-8') as file:
+    GetInfo(1)
+    with open('../texts/events.txt', encoding='utf-8') as file:
         send_mess = file.read()
 
-    bot.send_message(message.chat.id, send_mess, parse_mode='Markdown', reply_markup=markup)
+    bot.send_message(message.chat.id, send_mess, parse_mode='Markdown', reply_markup=markup,
+                     disable_web_page_preview=True)
 
 
 @bot.message_handler()
-def choise5(message):
+def choise2(message):
     markup = types.InlineKeyboardMarkup()
     markup.row_width = 1
     btn = types.InlineKeyboardButton('Назад', callback_data="yes")
     markup.add(btn)
 
-    with open('../texts/UMNIC_comprtition.txt', encoding='utf-8') as file:
+    GetInfo(2)
+    with open('../texts/public_services.txt', encoding='utf-8') as file:
+        send_mess = file.read()
+
+    bot.send_message(message.chat.id, send_mess, parse_mode='Markdown', reply_markup=markup,
+                     disable_web_page_preview=True)
+
+
+@bot.message_handler()
+def choise3(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.row_width = 1
+    btn = types.InlineKeyboardButton('Назад', callback_data="yes")
+    markup.add(btn)
+
+    with open('../texts/residents_competition.txt', encoding='utf-8') as file:
         send_mess = file.read()
 
     bot.send_message(message.chat.id, send_mess, parse_mode='Markdown', reply_markup=markup)
@@ -60,46 +99,29 @@ def choise4(message):
 
 
 @bot.message_handler()
-def choise3(message):
+def choise5(message):
     markup = types.InlineKeyboardMarkup()
     markup.row_width = 1
     btn = types.InlineKeyboardButton('Назад', callback_data="yes")
     markup.add(btn)
 
-    with open('../texts/residents_competition.txt', encoding='utf-8') as file:
+    with open('../texts/UMNIC_comprtition.txt', encoding='utf-8') as file:
         send_mess = file.read()
 
     bot.send_message(message.chat.id, send_mess, parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.message_handler()
-def choise2(message):
+def choise6(message):
     markup = types.InlineKeyboardMarkup()
     markup.row_width = 1
     btn = types.InlineKeyboardButton('Назад', callback_data="yes")
     markup.add(btn)
 
-    GetInfo(2)
-    with open('../texts/public_services.txt', encoding='utf-8') as file:
+    with open('../texts/young_voter_competition.txt', encoding='utf-8') as file:
         send_mess = file.read()
 
-    bot.send_message(message.chat.id, send_mess, parse_mode='Markdown', reply_markup=markup,
-                     disable_web_page_preview=True)
-
-
-@bot.message_handler()
-def choise1(message):
-    markup = types.InlineKeyboardMarkup()
-    markup.row_width = 1
-    btn = types.InlineKeyboardButton('Назад', callback_data="yes")
-    markup.add(btn)
-
-    GetInfo(1)
-    with open('../texts/events.txt', encoding='utf-8') as file:
-        send_mess = file.read()
-
-    bot.send_message(message.chat.id, send_mess, parse_mode='Markdown', reply_markup=markup,
-                     disable_web_page_preview=True)
+    bot.send_message(message.chat.id, send_mess, parse_mode='Markdown', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -120,27 +142,11 @@ def callback_inline(call):
         choise1(call.message)
 
 
-@bot.message_handler(commands=['about'])
-def about(message):
-    markup = types.InlineKeyboardMarkup()
-    markup.row_width = 1
-    btn1 = types.InlineKeyboardButton('Ближайшие мероприятия', callback_data="choise1")
-    btn2 = types.InlineKeyboardButton('Государственные услуги', callback_data="choise2")
-    btn3 = types.InlineKeyboardButton('Как стать резидентом Технопарка Югры', callback_data="choise3")
-    btn4 = types.InlineKeyboardButton('Акселератор технологических стартапов', callback_data="choise4")
-    btn5 = types.InlineKeyboardButton('Конкурс «УМНИК»', callback_data="choise5")
-    btn6 = types.InlineKeyboardButton('Конкурс «Молодой изобретатель Югры»', callback_data="choise6")
-    markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
-
-    send_mess = f"Что Вас интересует?"
-    bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
-
-
 @bot.message_handler(content_types=['text'], func=lambda call: True)
-def commands():
+def commands(message):
     while True:
         # Получаем старый текстовый файл с новостью, перед обновлением
-        old_news = open('texts/relevant_news.txt', encoding='utf8')
+        old_news = open('../texts/relevant_news.txt', encoding='utf8')
         old_text = ''.join(old_news.readlines())
 
         # Обновляем текстовый файл: загружаем в него текст последней новости
@@ -150,7 +156,7 @@ def commands():
             continue
 
         # Загружаем обновлённый текстовый файл, получаем из него текст:
-        update_news = open('texts/relevant_news.txt', encoding='utf8')
+        update_news = open('../texts/relevant_news.txt', encoding='utf8')
         update_text = update_news.readlines()
 
         # Если новость обновилась:
